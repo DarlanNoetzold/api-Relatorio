7798a976abede4455bdf4ad9b177f85cf0f835d9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-10-18 10:26:06 -0300

    Initial Project

---
+++ api-spring/src/test/java/tech/noetzold/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
